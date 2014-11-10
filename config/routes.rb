a5bb17e1873b46ed363da00a75c1fec4c85741de
Nan Davenport <nannigan@nannigans.com>
2014-11-10 22:00:53 +0000

    added devise, bootstrap-sass, simple_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Chesster::Application.routes.draw do
 
+  devise_for :users
+  devise_for :views
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
