20ed815811571a122707fed24bd69430f67ef8ee
Ilya Krasnov <ilya.krasnov@gmail.com>
2014-11-21 09:23:12 +0000

    Create games controller and define show view

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,194 +5,194 @@ class Game < ActiveRecord::Base
 	def initialize_the_board!
 		self.pieces.create(
 			:color => 'white',
-			:x => 0,
-			:y => 0,
+			:x_coord => 0,
+			:y_coord => 0,
 			:piece_type => 'rook'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 1,
-			:y => 0,
+			:x_coord => 1,
+			:y_coord => 0,
 			:piece_type => 'knight'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 2,
-			:y => 0,
+			:x_coord => 2,
+			:y_coord => 0,
 			:piece_type => 'bishop'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 3,
-			:y => 0,
+			:x_coord => 3,
+			:y_coord => 0,
 			:piece_type => 'queen'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 4,
-			:y => 0,
+			:x_coord => 4,
+			:y_coord => 0,
 			:piece_type => 'king'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 5,
-			:y => 0,
+			:x_coord => 5,
+			:y_coord => 0,
 			:piece_type => 'bishop'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 6,
-			:y => 0,
+			:x_coord => 6,
+			:y_coord => 0,
 			:piece_type => 'knight'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 7,
-			:y => 0,
+			:x_coord => 7,
+			:y_coord => 0,
 			:piece_type => 'rook'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 0,
-			:y => 1,
+			:x_coord => 0,
+			:y_coord => 1,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 1,
-			:y => 1,
+			:x_coord => 1,
+			:y_coord => 1,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 2,
-			:y => 1,
+			:x_coord => 2,
+			:y_coord => 1,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 3,
-			:y => 1,
+			:x_coord => 3,
+			:y_coord => 1,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 4,
-			:y => 1,
+			:x_coord => 4,
+			:y_coord => 1,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 5,
-			:y => 1,
+			:x_coord => 5,
+			:y_coord => 1,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 6,
-			:y => 1,
+			:x_coord => 6,
+			:y_coord => 1,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'white',
-			:x => 7,
-			:y => 1,
+			:x_coord => 7,
+			:y_coord => 1,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 0,
-			:y => 6,
+			:x_coord => 0,
+			:y_coord => 6,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 1,
-			:y => 6,
+			:x_coord => 1,
+			:y_coord => 6,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 2,
-			:y => 6,
+			:x_coord => 2,
+			:y_coord => 6,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 3,
-			:y => 6,
+			:x_coord => 3,
+			:y_coord => 6,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 4,
-			:y => 6,
+			:x_coord => 4,
+			:y_coord => 6,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 5,
-			:y => 6,
+			:x_coord => 5,
+			:y_coord => 6,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 6,
-			:y => 6,
+			:x_coord => 6,
+			:y_coord => 6,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 7,
-			:y => 6,
+			:x_coord => 7,
+			:y_coord => 6,
 			:piece_type => 'pawn'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 0,
-			:y => 7,
+			:x_coord => 0,
+			:y_coord => 7,
 			:piece_type => 'rook'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 1,
-			:y => 7,
+			:x_coord => 1,
+			:y_coord => 7,
 			:piece_type => 'knight'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 2,
-			:y => 7,
+			:x_coord => 2,
+			:y_coord => 7,
 			:piece_type => 'bishop'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 3,
-			:y => 7,
+			:x_coord => 3,
+			:y_coord => 7,
 			:piece_type => 'queen'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 4,
-			:y => 7,
+			:x_coord => 4,
+			:y_coord => 7,
 			:piece_type => 'king'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 5,
-			:y => 7,
+			:x_coord => 5,
+			:y_coord => 7,
 			:piece_type => 'bishop'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 6,
-			:y => 7,
+			:x_coord => 6,
+			:y_coord => 7,
 			:piece_type => 'knight'
 			)
 		self.pieces.create(
 			:color => 'black',
-			:x => 7,
-			:y => 7,
+			:x_coord => 7,
+			:y_coord => 7,
 			:piece_type => 'rook'
 			)
 	end
