ff5fdbcf6c33b6b0613b82b7e8fd1023eeb3cf80
Brant Wellman <brantwellman@gmail.com>
2014-12-08 16:31:54 +0000

    Select page, routes.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,27 @@
 class GamesController < ApplicationController
+  
   def show
   	@game = Game.find(params[:id])
   	@pieces = @game.pieces
   end
+
+
+
+  def select
+  	# add code to links identifying each individual piece
+    # when user clicks link, pass x,y into url
+    @piece = Piece.find(params[:id, :x_coord, :y_coord])
+    # redirect to select page that displays piece with that x, y
+
+  end
+
+
+  def move
+  	# Nan's assignment
+
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Chesster::Application.routes.draw do
   get "game/show"
   resources :games
   devise_for :users
+  get 'games/:id/:x/:y', :to => 'games#select', :as => 'select'
+  # get 'games/select'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
