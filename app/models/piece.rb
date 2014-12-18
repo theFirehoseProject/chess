1a16681d3a1307efbcc942d9e7a15fa159e598f5
Rehan Ali <ali.bobby@gmail.com>
2014-12-18 16:46:47 +0000

    Moved move logic from Game model to Piece model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,49 @@
 class Piece < ActiveRecord::Base
 	belongs_to :game
+
+
+	def is_move_allowed?(new_x, new_y)
+		# need to check the following:
+		#   - is move permitted for that piece according to piece rules?
+		# 	- is the move obstructed by another piece?
+		piece = Piece.find(self.id)
+		if piece.piece_type == 'pawn'
+			return legit_moves_for_pawn.include? [new_x, new_y]
+		end
+	end
+
+	def legit_moves_for_pawn
+		# currently checks for boundary, and 
+		# directionality based on color
+		#
+		# does NOT currently check the following:
+		# - obstructing piece
+		# - only move 2 places on first move
+		# - move diagonal to take another piece
+		piece = Piece.find(self.id)
+		x_init = piece.x_coord
+		y_init = piece.y_coord
+		color  = piece.color
+
+		if color == "black"
+			if y_init == 0
+				return []
+			elsif y_init == 1
+				return [ [x_init, y_init - 1] ]
+			else
+				return [ [x_init, y_init - 1], 
+						 [x_init, y_init - 2] ]
+			end
+		else
+			if y_init == 7
+				return []
+			elsif y_init == 6
+				return [ [x_init, y_init + 1] ]
+			else
+				return [ [x_init, y_init + 1], 
+						 [x_init, y_init + 2] ]
+			end
+		end
+	end
+
 end
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class PieceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "check pawn moves are legit" do 		
+		piece = FactoryGirl.create(:piece, :x_coord => 3, :y_coord => 4, :color => "white", :piece_type => "pawn")
+		actual = piece.is_move_allowed?(3, 5)
+		assert actual, "White pawns move up the board 1 or 2 places"
+
+		actual = piece.is_move_allowed?(3, 7)
+		assert_not actual, "White pawns don't move more than 2 places up"
+
+		actual = piece.is_move_allowed?(3, 2)
+		assert_not actual, "White pawns don't move backwards (down the board)"
+
+		actual = piece.is_move_allowed?(4, 5)
+		assert_not actual, "White pawns don't move sideways"
+
+		piece = FactoryGirl.create(:piece, :x_coord => 3, :y_coord => 4, :color => "black", :piece_type => "pawn")
+		actual = piece.is_move_allowed?(3, 3)
+		assert actual, "Black pawns move down the board 1 or 2 places"
+
+		actual = piece.is_move_allowed?(3, 0)
+		assert_not actual, "Black pawns don't move more than 2 places up"
+
+		actual = piece.is_move_allowed?(3, 6)
+		assert_not actual, "Black pawns don't move backwards (up the board)"
+
+		actual = piece.is_move_allowed?(4, 3)
+		assert_not actual, "Black pawns don't move sideways"
+
+		# add tests to ensure pieces don't move off the board
+
+	end
+
 end
