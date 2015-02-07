fb796584ec6ac5e8326601d12c992d1d03e0a4ad
Nan Davenport <nannigan@nannigans.com>
2015-02-06 16:35:39 -0800

    Update game.js
    removed uneeded code

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,19 +1,1 @@
-// $(function() {
-	
-	// var dragDrop = function(path){
-	//     $(".moveable").draggable();
-	// 			$("td.space").droppable({
-	// 				drop: function( event, ui ) {
-	// 					var drop_target_id = event.target.id;
-	// 					var target_x = drop_target_id[0];
-	// 					var target_y = drop_target_id[1];
-	// 					var piece_id = ui.draggable.prop('id');
-	// 					var $form =  $('#move');
-	// 					$form.attr('action', path);
-	// 					console.log(path);
-	// 					$form.submit();
 
-	// 		      }
-	// 			});
-	// 		};
-  // });
